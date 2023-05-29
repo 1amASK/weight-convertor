6b9a7862c66c829cb92622a3ed11b76fd82ae56c
ASK <lucykenos@icloud.com>
2023-05-30 07:48:09 +0930

    Added Weight Convertor File

---
+++ Weight Convertor.py
@@ -0,0 +1,11 @@
+"""
+    Write a program which:
+        1. Prompts the user for the weight they want to convert.
+            "Weight: "
+        2. Asks what it wants to convert to.
+            "Convert to (K)gs or (L)bs? "
+        3. Gives the correct conversion.
+"""
+def convert(weight):
+    pass
+weight = int(input("Weight: "))
